fc6d11ec9da5a451bb4061706feffd3f28be858c
DharnikasriMuruganandam <dharanikasri140@gmail.com>
2026-02-26 12:32:47 +0530

    Create swapnodes.py

---
+++ python/swapnodes.py
@@ -0,0 +1,20 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        
+        while prev.next and prev.next.next:
+            first_node = prev.next
+            second_node = prev.next.next
+            prev.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+            prev = first_node
+            
+        return dummy.next
