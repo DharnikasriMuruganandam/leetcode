b9b27ea7420f8ab4a17be6a4c973f37fd5e466a2
DharnikasriMuruganandam <dharanikasri140@gmail.com>
2026-02-26 15:00:42 +0530

    Update rotation.py

--- python/rotation.py
+++ python/rotation.py
@@ -11,15 +11,13 @@ class Solution:
             # Identify which half is sorted
             # Case 1: Left half [low...mid] is sorted
             if nums[low] <= nums[mid]:
-                # Check if target is within the sorted left half
                 if nums[low] <= target < nums[mid]:
                     high = mid - 1
                 else:
                     low = mid + 1
             
-            # Case 2: Right half [mid...high] is sorted
+           
             else:
-                # Check if target is within the sorted right half
                 if nums[mid] < target <= nums[high]:
                     low = mid + 1
                 else:
