417d77eda1e618b4c98e992601a7df108c21bf0a
DharnikasriMuruganandam <dharanikasri140@gmail.com>
2026-02-26 15:20:12 +0530

    Create mergesortedlist.py

---
+++ python/mergesortedlist.py
@@ -0,0 +1,21 @@
+import heapq
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        current = dummy
+        min_heap = []
+        
+        for i, l in enumerate(lists):
+            if l:
+                heapq.heappush(min_heap, (l.val, i, l))
+        
+        while min_heap:
+            val, i, node = heapq.heappop(min_heap)
+            current.next = node
+            current = current.next
+            
+            if node.next:
+                heapq.heappush(min_heap, (node.next.val, i, node.next))
+                
+        return dummy.next
