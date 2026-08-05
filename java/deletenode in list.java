4feb592648cc37f1c8f1e7660fba02550993b83e
DharnikasriMuruganandam <dharanikasri140@gmail.com>
2026-08-05 12:10:30 +0530

    Create deletenode in list.java

---
+++ java/deletenode in list.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+
+        while (current.next != null) {
+            if (set.contains(current.next.val)) {
+                current.next = current.next.next;
+            } else {
+                current.next = current.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
