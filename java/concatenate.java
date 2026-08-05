5a5555e251b7ea7c4b2a96daee6e3c9ed23ff18b
DharnikasriMuruganandam <dharanikasri140@gmail.com>
2026-08-05 12:27:28 +0530

    Create concatenate.java

---
+++ java/concatenate.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        List<String> result = new ArrayList<>();
+        HashSet<String> set = new HashSet<>(Arrays.asList(words));
+
+        for (String word : words) {
+            set.remove(word);
+            if (canForm(word, set)) {
+                result.add(word);
+            }
+            set.add(word);
+        }
+
+        return result;
+    }
+
+    private boolean canForm(String word, HashSet<String> set) {
+        if (word.isEmpty()) {
+            return false;
+        }
+
+        int n = word.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(word.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
