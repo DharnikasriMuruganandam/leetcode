bf804af0ab5d099d9f0e85e0a6b836c8ecfe9df7
DharnikasriMuruganandam <dharanikasri140@gmail.com>
2026-08-05 10:11:31 +0530

    Create list to binary tree.java

---
+++ java/list to binary tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        // Base case: if the list is empty
+        if (head == null) {
+            return null;
+        }
+        // Base case: if there's only one node left
+        if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+
+        // Find the middle element using slow and fast pointers
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Disconnect the left half of the list from the middle node
+        if (prev != null) {
+            prev.next = null;
+        }
+
+        // Create the root node with the middle element's value
+        TreeNode root = new TreeNode(slow.val);
+
+        // Recursively build the left and right subtrees
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+
+        return root;
+    }
+}
